bfade1fcedbf608ae7ecad1984a772e7aad9ea77
MIkhail <mmv-774@yandex.ru>
2022-02-10 19:42:18 +0500

    заполнить данные пользователя

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { api } from '../utils/api.js';
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+  const [userName, setUserName] = useState(null);
+  const [userDescription, setUserDescription] = useState(null);
+  const [userAvatar, setUserAvatar] = useState(null);
+
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then((res) => {
+        setUserName(res.name);
+        setUserDescription(res.about);
+        setUserAvatar(res.avatar);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <main className='content'>
       <section className='profile horizontal-aligned-block'>
         <div className='profile__avatar-overlay'>
-          <img src='#' alt='Аватар пользователя.' className='profile__avatar' />
+          <img src={`${userAvatar}`} alt='Аватар пользователя.' className='profile__avatar' />
           <button className='profile__btn profile__btn_action_avatar-edit' onClick={onEditAvatar}></button>
         </div>
         <div className='profile__info'>
-          <h1 className='profile__title'></h1>
+          <h1 className='profile__title'>{userName}</h1>
           <button
             type='button'
             className='profile__btn profile__btn_action_edit animated-element'
             onClick={onEditProfile}
           ></button>
-          <p className='profile__subtitle'></p>
+          <p className='profile__subtitle'>{userDescription}</p>
         </div>
         <button
           type='button'
